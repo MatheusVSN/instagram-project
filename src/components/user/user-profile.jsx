3c9f8d9232597771bc55bfd9f0263eec8b1aa0af
Matheus <matheusvsn2005@gmail.com>
2023-04-15 15:43:06 -0300

    set size on profile

--- src/components/user/user-profile.jsx
+++ src/components/user/user-profile.jsx
@@ -5,7 +5,7 @@ export default function UserProfile({ ImageSource, Name }) {
         <div className="rounded-full transition hover:cursor-pointer hover:font-bold">
             <div className="p-0.5 bg-gradient-to-tr from-amber-500 to-fuchsia-700 rounded-full w-[61px]">
                 <div className="p-1 bg-white rounded-full w-full h-full">
-                    <Image className="rounded-full" src={ImageSource} alt={Name} width={50} height={50} />
+                    <Image className="rounded-full w-[50px] h-[50px]" src={ImageSource} alt={Name} width={50} height={50} />
                 </div>
             </div>
             <p className="text-center text-xs mt-2">{Name}</p>
